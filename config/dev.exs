c6eb1273e45f52803397ca251d3757b94495f51b
sauravkr20 <sauravkr20@iitk.ac.in>
2024-10-15 01:31:39 +0530

    directory stryuct

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :plax, Plax.Repo,
 config :plax, PlaxWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4500],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
