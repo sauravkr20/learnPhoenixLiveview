41914841f962dd3cbb5d43d0348b4b83d4dadc48
sauravkr20 <sauravkr20@iitk.ac.in>
2024-10-15 18:01:04 +0530

    list all rooms in sidebar

--- lib/plax_web/live/chat_room_live.ex
+++ lib/plax_web/live/chat_room_live.ex
@@ -6,6 +6,26 @@ defmodule PlaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+      <div class="flex flex-col flex-shrink-0 bg-slate-100">
+        <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-lg font-bold text-gray-800">
+              Slax
+            </h1>
+          </div>
+        </div>
+        <div class="mt-4 overflow-auto">
+          <div class="flex items-center h-8 px-3 group">
+            <span class="ml-2 leading-none font-medium text-sm"> Rooms</span>
+          </div>
+          <div id="rooms-list">
+            <%!-- <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} /> --%>
+            <%= for room <- @rooms do %>
+              <%= room_link(%{room: room, active: room.id == @room.id}) %>
+            <% end %>
+          </div>
+        </div>
+      </div>
       <div class="flex flex-col flex-grow shadow-lg">
         <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
           <div class="flex flex-col gap-1.5">
@@ -25,12 +45,27 @@ defmodule PlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :active , :boolean, required: true
+  attr :room, Room, required: true
+  defp room_link(assigns) do
+    ~H"""
+      <a class={["flex items-center h-8 px-3 group", (@active && "bg-slate-300") || "hover:bg-slate-300" ]}>
+        <.icon name="hero-hashtag" class="h-4 w-4"/>
+        <span class={["ml-2 leading-none", @active && "font-bold"]}>
+          <%= @room.name %>
+        </span>
+      </a>
+    """
+  end
+
   def mount(_params, _session, socket) do
-    room = Room |> Repo.all() |> List.first()
-    {:ok, assign(socket, hide_topic?: false, room: room)}
+    rooms = Room |> Repo.all()
+    room = rooms |> List.first()
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _, socket) do
-    {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
+    # {:noreply, assign(socket, hide_topic?: !socket.assigns.hide_topic?)}
+    {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 end
