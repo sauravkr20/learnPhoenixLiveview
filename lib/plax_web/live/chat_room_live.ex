0e22cc47a66c157b05afd56db3fbf2c73c0caeb4
sauravkr20 <sauravkr20@iitk.ac.in>
2024-10-15 06:35:22 +0530

    show room info in ChatRoom Live

--- lib/plax_web/live/chat_room_live.ex
+++ lib/plax_web/live/chat_room_live.ex
@@ -1,11 +1,28 @@
 defmodule PlaxWeb.ChatRoomLive do
   use PlaxWeb, :live_view
 
+  alias Plax.Chat.Room
+  alias Plax.Repo
+
   def render(assigns) do
     ~H"""
-      <div>Welcome to the Chat!</div>
-      <%= "<div>Welcome to \n the chat!</div>" %>
-      <%= raw "<div>Welcome \n to the chat!</div>" %>
+      <div class="flex flex-col flex-grow shadow-lg">
+        <div class="flex justify-between items-center flex-shrink-0 h-16 bg-white border-b border-slate-300 px-4">
+          <div class="flex flex-col gap-1.5">
+            <h1 class="text-sm font-bold leading-none">
+              <%= @room.name%>
+            </h1>
+            <div class="text-xs leading-none h-3.5"> <%= @room.topic %></div>
+          </div>
+        </div>
+      </div>
     """
+  end
+
+  def mount(_params, _session, socket) do
+    room = Room |> Repo.all() |> List.first()
+    socket = socket
+              |> assign(:room, room)
+    {:ok, socket}
   end
 end
