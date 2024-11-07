444240821374ca6308090c8f4c79557fe5493847
sauravkr20 <sauravkr20@iitk.ac.in>
2024-11-07 17:34:01 +0530

    only show delete button on hover

--- lib/plax_web/live/chat_room_live.ex
+++ lib/plax_web/live/chat_room_live.ex
@@ -126,10 +126,10 @@ defmodule PlaxWeb.ChatRoomLive do
   attr :message, Message, required: true
   defp message(assigns) do
     ~H"""
-    <div id={@dom_id} class="relative flex px-4 py-3">
+    <div id={@dom_id} class="relative flex px-4 py-3 group">
     <button
         :if={@current_user.id == @message.user_id}
-        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer hidden group-hover:block"
         data-configm="Are you sure?"
         phx-click="delete-message"
         phx-value-id={@message.id}
