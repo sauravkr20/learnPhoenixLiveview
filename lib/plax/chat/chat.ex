4189a49fa742deb93f4c44d1e52e8a5cbfa2232b
sauravkr20 <sauravkr20@iitk.ac.in>
2024-11-07 14:45:25 +0530

    users can send messages to the chat

--- lib/plax/chat/chat.ex
+++ lib/plax/chat/chat.ex
@@ -43,4 +43,14 @@ defmodule Plax.Chat do
       |> order_by(asc: :inserted_at)
       |> Repo.all()
   end
+
+  def change_message(message, attrs \\ %{}) do
+    Message.changeset(message, attrs)
+  end
+
+  def create_message(room , attrs, user) do
+    %Message{room: room, user: user}
+    |> Message.changeset(attrs)
+    |> Repo.insert()
+  end
 end
